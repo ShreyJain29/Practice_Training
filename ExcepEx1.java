b14f62500d342944fb7da89f191788f84dd19b0a
ShreyJain29 <64628407+ShreyJain29@users.noreply.github.com>
2022-07-14 15:15:57 +0530

    Add files via upload

---
+++ ExcepEx1.java
@@ -0,0 +1,25 @@
+package excep;
+public class ExcepEx1 {
+	int a = 10;
+	int b = 0;
+	int c[] = new int[5]; 
+	int excep()
+	{
+		try 
+		{
+			return a/b;				
+		}	
+		catch(ArithmeticException ae) 
+		{
+			return 1;
+		}
+		finally 
+		{
+		System.out.println("After Exception");
+		}
+	}
+	public static void main(String[] args) {
+		ExcepEx1 e = new ExcepEx1();
+		System.out.println(e.excep());
+	}
+}
